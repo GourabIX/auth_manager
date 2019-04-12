e9686351e5d8eb9ef5cbee212626a58a1d7585bb
Gourab Sarkar <GourabIX@users.noreply.github.com>
2019-04-12 14:09:54 +0530

    Updated auth_manager Version : 0.1.1
    Special handling for OTP generation
    In case of wrong argument entry, auth_manager now specifies what went wrong.

--- password_manager.py
+++ password_manager.py
@@ -1,14 +1,14 @@
-# ============================================================================================================================================= #
-# THIS PASSWORD MANAGER HAS BEEN BREWED WITH A LOT OF LOVE BY GOURAB SARKAR. IT USES A COMPLEX MIX OF STRONG POTIONS THAT GENERATE              #
-# ALOHOMORA - SAFE (CRYPTOGRAPHICALLY SECURE) PASSWORDS AND STORES THEM FOR YOU.                                                                #
-#                                                                                                                                               #
-# THIS PROGRAM ALONG WITH IT'S COMPLETE SOURCE CODE IS FREE TO USE, MODIFY AND DISTRIBUTE. I WOULD ONLY REQUEST YOU, THE USER, TO               # 
-# INCLUDE A CREDIT TO MY NAME WHEN YOU DO:                                                                                                      #
-# MADE WITH LOVE BY GOURAB SARKAR (https://github.com/GourabIX)                                                                                 #
-# ============================================================================================================================================= #
+# ================================================================================================================================================================= #
+# AUTH_MANAGER IS A PASSWORD GENERATOR AND MANAGER HAS BEEN BREWED WITH A LOT OF LOVE BY GOURAB SARKAR. IT USES A COMPLEX MIX OF STRONG POTIONS                     #
+# THAT GENERATE ALOHOMORA - SAFE (CRYPTOGRAPHICALLY SECURE) PASSWORDS AND STORES THEM FOR YOU.                                                                      #                                  #
+#                                                                                                                                                                   #
+# THIS PROGRAM ALONG WITH IT'S COMPLETE SOURCE CODE IS FREE TO USE, MODIFY AND DISTRIBUTE. I WOULD ONLY REQUEST YOU, THE USER, TO                                   # 
+# INCLUDE A CREDIT TO MY NAME WHEN YOU DO:                                                                                                                          #
+# MADE WITH LOVE BY GOURAB SARKAR (https://github.com/GourabIX)                                                                                                     #
+# ================================================================================================================================================================= #
 
 
-# GRAB PASSBOT'S FRIENDS
+# GRAB AUTH_MANAGER'S FRIENDS
 import string
 import random
 import sys
@@ -20,7 +20,7 @@ from tweaker import pass_customizer
 # DRESS FOR THE JOB YOU WANT!
 print("| -------------------------------------------------------------------- |")
 print("|                       Welcome to Auth_Manager!                       |")
-print("|                               v 0.1.0                                |")
+print("|                               v 0.1.1                                |")
 print("| -------------------------------------------------------------------- |")
 
 # THE MAIN FUNCTION, LITERALLY
@@ -34,6 +34,7 @@ if __name__ == '__main__':
         # GET THE CONFIG FROM USER
         print("In the next line, enter your password complexity preferences, separated by spaces. ")
         print("Usage : <how_many> <password_length> <symbols_enabled> <characters_enabled> <numerics_enabled> ")
+        print("If you want to generate OTPs, enter only <how_many> and <numerics_enabled> fields, and fill the rest with 0.")
         print("Sample entry [1 = enable, 0 = disable] : 6 24 1 1 0")
         qty, length, sym_en, char_en, num_en = map(int, input().split(' '))
         print("Okay... Got your password customisation preferences!\n")
@@ -44,7 +45,10 @@ if __name__ == '__main__':
         if err_status != 0:
             print("Something went wrong. Sorry for the hiccup. Do Try again!")
 
-    if adv_en == "n":
+    elif adv_en == "n":
         err_status = gen_pgp()
         if err_status != 0:
             print("Something went wrong. Sorry for the hiccup. Do Try again!")
+    
+    else:
+        print("Hmm... You entered a wrong argument. Please, try again! :)")
