210a8c09afd26563046a333346fc56794cf68c68
Gourab Sarkar <GourabIX@users.noreply.github.com>
2019-04-15 13:52:05 +0530

    Updated tp version : 0.2.1
    Handled 2 security issues
    UX improvements
    Optimised sections of code

--- password_manager.py
+++ password_manager.py
@@ -12,17 +12,21 @@
 import string
 import random
 import sys
-sys.path.append("C:/Users/gsarkar/Documents/Code/auth_manager/engine")
+import atexit
+import time
+sys.path.append("./engine")
 from hvp import gen_hvp
 from pgp import gen_pgp
 from tweaker import pass_customizer
+from imprint import *
 from secure import *
 
 # DRESS FOR THE JOB YOU WANT!
-print("| -------------------------------------------------------------------- |")
-print("|                       Welcome to Auth_Manager!                       |")
-print("|                               v 0.1.5                                |")
-print("| -------------------------------------------------------------------- |")
+print("                 | -------------------------------------------------------------------- |")
+print("                 |                       Welcome to Auth_Manager!                       |")
+print("                 |                               v 0.2.1                                |")
+print("                 | -------------------------------------------------------------------- |")
+print("\n")
 
 # THE MAIN FUNCTION, LITERALLY
 if __name__ == '__main__':
@@ -50,19 +54,20 @@ if __name__ == '__main__':
                 print("Something went wrong. Sorry for the hiccup. Do Try again!")
 
         elif adv_en == "n":
-            # TODO : DECRYPT FILE FIRST
             err_status = gen_pgp(site)
             if err_status != 0:
                 print("Something went wrong. Sorry for the hiccup. Do Try again!")
-            entry_point(49)
-            # TODO : DELETE CSV FILE FROM DISK
-        
+            
+            purger()                # PURGE ALL REDUNDANT DATA FILES
+
         else:
             print("Hmm... You entered a wrong argument. Please, try again! :)")
     
     elif splash == 9:
         # TODO : Deal with Decryption in secure.py
         entry_point(69)                                         # WOW! *NODS IN DISBELIEF; LAUGHS IN entry_point*
+        open_vault()
+        exit_action()
     
     else:
         print("What was that? Please try again.")
